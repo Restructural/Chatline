b524b172f2cf522974d6add5eef4cf078fb3d3f1
CD <BE@imac.local>
2018-02-04 18:22:00 +0000

    Initial Commit

---
+++ Chatline/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Chatline
+//
+//  Created by BE on 2018-02-04.
+//  Copyright © 2018 Restructural. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
