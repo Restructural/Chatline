82ff54941de4fe1f04eea88ecb81207b46a6a597
CD <BE@iMac.local>
2018-02-11 16:39:48 +0000

    lesson 10: crearte account modal ui

--- Chatline/View/ModalCreateAccount.swift
+++ Chatline/View/ModalCreateAccount.swift
@@ -12,7 +12,15 @@ class ModalCreateAccount: NSView {
 
     // Outlets
     @IBOutlet weak var view : NSView!
-
+    @IBOutlet weak var nameText: NSTextField!
+    @IBOutlet weak var emailText: NSTextField!
+    @IBOutlet weak var passwordText: NSSecureTextField!
+    @IBOutlet weak var profileImage: NSImageView!
+    @IBOutlet weak var createAccountButton: NSButton!
+    @IBOutlet weak var chooseImageButton: NSButton!
+    @IBOutlet weak var progressSpinner: NSProgressIndicator!
+    @IBOutlet weak var stackView: NSStackView!
+    
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
         Bundle.main.loadNibNamed(NSNib.Name(rawValue: "ModalCreateAccount"), owner: self, topLevelObjects: nil)
@@ -29,7 +37,16 @@ class ModalCreateAccount: NSView {
         setUpView()
     }
     
-
+    
+    @IBAction func closeModalClicked(_ sender: Any) {
+        NotificationCenter.default.post(name: NOTIF_CLOSE_MODAL, object: nil)
+    }
+    
+    @IBAction func createAccountButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func chooseImageButtonClicked(_ sender: Any) {
+    }
     
     func setUpView() {
         self.view.frame = NSRect(x: 0, y: 0, width: 475, height: 300)
@@ -37,6 +54,25 @@ class ModalCreateAccount: NSView {
         view.layer?.cornerRadius = 7
         
         // Button Styling   (connect iboutlets and actions first)
+        
+        profileImage.layer?.cornerRadius = 10
+        profileImage.layer?.borderColor = NSColor.gray.cgColor
+        profileImage.layer?.borderWidth = 3
+        
+        createAccountButton.layer?.backgroundColor = chatGreen.cgColor
+        chooseImageButton.layer?.backgroundColor = chatGreen.cgColor
+        
+        createAccountButton.layer?.cornerRadius = 7
+        chooseImageButton.layer?.cornerRadius = 7
+        
+        createAccountButton.styleButtonText(button: createAccountButton, buttonName: "Create Account", fontColor: .white
+        , alignment: .center, font: HELVETICA_BOLD, size: 13.0)
+        chooseImageButton.styleButtonText(button: chooseImageButton, buttonName: "Choose Avatar", fontColor: .white, alignment: .center, font: HELVETICA_BOLD, size: 13.0)
+        
+        nameText.focusRingType = .none
+        emailText.focusRingType = .none
+        passwordText.focusRingType = .none
+        
 
     }
 
